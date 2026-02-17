a3d2860cc2001c4a35fac4dc165d4f8d70304fea
Yorick van der Vis <yorick@fastmail.com>
2026-02-17 14:21:42 +0100

    feat: sentry error tracking (client + server, errors only)

---
+++ src/hooks.client.ts
@@ -0,0 +1,11 @@
+import * as Sentry from '@sentry/sveltekit';
+import { env } from '$env/dynamic/public';
+
+if (env.PUBLIC_SENTRY_DSN) {
+	Sentry.init({
+		dsn: env.PUBLIC_SENTRY_DSN,
+		tracesSampleRate: 0
+	});
+}
+
+export const handleError = Sentry.handleErrorWithSentry();
